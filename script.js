6bf525ec50b6fd727acd071f4d22bd194f1b7e79
Roman <kotelnikovrm@gmail.com>
2020-06-11 19:53:37 +0800

    переделал html, css, js
    переиминовал классы в html, css
    Изменил функцию operators

--- script.js
+++ script.js
@@ -4,38 +4,66 @@ function number(e) {
     var number_button;
     e = e || window.event;
     var el = e.target || e.srcElement;
-    
-    if(el.id == "button_zero") {
+
+    if (el.id == "btn_zero") {
         number_button = 0;
     }
-    else if(el.id == "button_one") {
+    else if (el.id == "btn_one") {
         number_button = 1;
     }
-    else if(el.id == "button_two") {
+    else if (el.id == "btn_two") {
         number_button = 2;
     }
-    else if(el.id == "button_three") {
+    else if (el.id == "btn_three") {
         number_button = 3;
     }
-    else if(el.id == "button_four") {
+    else if (el.id == "btn_four") {
         number_button = 4;
     }
-    else if(el.id == "button_five") {
+    else if (el.id == "btn_five") {
         number_button = 5;
     }
-    else if(el.id == "button_six") {
+    else if (el.id == "btn_six") {
         number_button = 6;
     }
-    else if(el.id == "button_seven") {
+    else if (el.id == "btn_seven") {
         number_button = 7;
     }
-    else if(el.id == "button_eight") {
+    else if (el.id == "btn_eight") {
         number_button = 8;
     }
-    else if(el.id == "button_nine") {
+    else if (el.id == "btn_nine") {
         number_button = 9;
     }
     answer(number_button);
+}
+
+//операторы
+function operators(e) {
+    var operator;
+    e = e || window.event;
+    var el = e.target || e.srcElement;
+
+    if (el.id == "btn_plus") {
+        operator = "+";
+    }
+    else if (el.id == "btn_minus") {
+        operator = "-";
+    }
+    else if (el.id == "btn_multiply") {
+        operator = "X";
+    }
+    else if (el.id == "btn_divide") {
+        operator = "/";
+    }
+    else if (el.id == "btn_result") {
+        operator = "=";
+    }
+    else {
+        operator = "undefined";
+    }
+
+    answer(operator);
 }
 
 //сохранение значений
@@ -47,5 +75,3 @@ function answer(x) {
     //вывод
     input.value = example;
 }
-
-function operations() {}
